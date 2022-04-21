df35664c1997aa76a339e733b98599667e090072
Soranosuke2001 <sorazora1.4@gmail.com>
2022-04-21 12:49:43 -0700

    hides all the contacts and displays the contact selected

--- src/app.js
+++ src/app.js
@@ -54,14 +54,38 @@ const contactsList = [
 // Your code goes here
 // Displays all the cards
 const display_all_contacts_area = document.querySelector('#display_all_contacts')
+const hide_all_contacts = document.getElementById('display_all_contacts')
+const display_single_card = document.querySelector('#display_single_contact')
 
-for (const cards of contactsList) {
-  const card_name = cards.name
-  const image_source = cards.image
-  const single_card = `<div><img src='../img/${image_source}' alt='profile picture'><p>${card_name}</p></div>`
-  display_all_contacts_area.insertAdjacentHTML('beforeend', single_card)
+display_all_cards()
+
+function display_all_cards() {
+  for (const cards of contactsList) {
+    const card_name = cards.name
+    const image_source = cards.image
+    const card_id = cards.ID
+    const single_card = `<div id=${card_id}><img id=${card_id} src='../img/${image_source}' alt='profile picture'><p>${card_name}</p></div>`
+    display_all_contacts_area.insertAdjacentHTML('beforeend', single_card)
+  }
+  
 }
 
-// function display_all_cards (single_card) {
-//   display_all_contacts_area.insertAdjacentHTML('beforeend', single_card)
-// }
+// Displays the individual cards when clicked
+display_all_contacts_area.addEventListener('click', (evt) => {
+  for (const cards of contactsList) {
+    if (evt.target.id == cards.ID) {
+      hide_all_contacts.style.display = 'none'
+      show_singlecard(evt.target.id)
+    }
+  }
+})
+
+function show_singlecard(clicked_card) {
+  const card_id = contactsList[clicked_card]
+  const image_source = contactsList[clicked_card].image
+  const card_name = contactsList[clicked_card].name
+  const card_phone = contactsList[clicked_card].phone
+  const card_email = contactsList[clicked_card].email
+  single_card_display = `<div id='individual_contact'><img id=${card_id} src='../img/${image_source}'<brk><h1>${card_name}</h1><brk><p>${card_phone}</p><p>${card_email}</p><span></span></div>`
+  display_single_card.insertAdjacentHTML('beforeend', single_card_display)
+}
